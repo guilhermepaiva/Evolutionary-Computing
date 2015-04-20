7bcf922b6cb9cf6290499c868f3b23dd866c09b0
guilhermepaiva <guilhermepaiva9@gmail.com>
2015-04-20 20:36:18 -0300

    add mais funções

--- ag_miniproject.py
+++ ag_miniproject.py
@@ -8,14 +8,16 @@ def makechromosome(alleles, length_chromosome):
 def mutate(gene, alleles, chromosome):
 	chromosome[gene] = random.choice(alleles)
 
+def makepopulation(population_size, alleles, length_chromosome):
+	return [makechromosome(alleles, length_chromosome) for individual in range(population_size)]
+
 
 if __name__ == "__main__":
 	alleles = (0,1)
 	length_chromosome = 5
-	a = makechromosome(alleles, length_chromosome)
-	print "Cromossomo: " 
-	print a
-	print "\n"
-	print "Depois da Mutacao:"
-	mutate(2, alleles, a)
-	print a
+	population_size = 10
+	all_population = makepopulation(population_size, alleles, length_chromosome)
+	print "Populacao: "
+	print all_population
+	
+
