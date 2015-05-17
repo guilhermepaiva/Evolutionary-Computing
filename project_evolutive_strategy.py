51d6525bbd0c2f38d0a7e4039c6e96b064ec5b05
guilhermepaiva <guilhermepaiva9@gmail.com>
2015-05-17 11:50:17 -0300

    another functions

--- project_evolutive_strategy.py
+++ project_evolutive_strategy.py
@@ -1,5 +1,8 @@
 import random
 import math
+import numpy as np
+
+# TODO random.uniform sem timestamp
 
 def makechromosome():
 	length_chromosome = 31
@@ -10,7 +13,7 @@ def makechromosome():
 def makepopulation(population_size):
 	return [makechromosome() for individual in range(population_size)]
 
-def calculate_fitness(chromosome):
+def calculate_ackley(chromosome):
 	a, b, c = 20, 0.2, (math.pi)*2
 	first_sum = 0.0
 	for i in range(len(chromosome)-1):
@@ -24,9 +27,65 @@ def calculate_fitness(chromosome):
 	result = (-a * math.exp(first_sum)) - math.exp(second_sum) + a + math.exp(1)
 	return result
 
+def calculate_fitness(chromosome):
+	return 1.0/(calculate_ackley(chromosome) + 1.0)
+
+def check_fitness(population):
+	ideal_fitness = 0.9
+	for individual in population:
+		if calculate_fitness(individual) >= ideal_fitness:
+			return True
+	return False
+
+def mutate(chromosome):
+	threshold = 0.1
+	learning_tax = 1.0/math.sqrt(len(chromosome)-1)
+	normal_random = np.random.normal(0,1)
+	next_sigma = chromosome[-1]*math.exp(learning_tax*normal_random)
+	if next_sigma < threshold:
+		next_sigma = threshold
+	return [chromosome[i] + next_sigma*np.random.normal(0,1) for i in range(len(chromosome)-1)]
+
+
+def pick_pivots():
+	left = random.randrange(1, 29)
+	right = random.randrange(left, 30)
+	return left, right
+
+
+def crossover(mate1, mate2):
+	probability = 0.5
+	if random.uniform(0.0, 1.0) < probability:
+		return [((mate1[i] + mate2[i]) / 2.0) for i in range(len(mate1)-1)]
+	else:
+		left, right = pick_pivots()
+		child1 = mate1[:left] + mate2[left:]
+		child2 = mate2[:left] + mate1[left:]
+		if calculate_fitness(child1) > calculate_fitness(child2):
+			return child1
+		else:
+			return child2
+
+
+#def next_generation(population):
+
+	
+
+
+
 
 
 if __name__ == "__main__":
 	my_population = makepopulation(10)
-	for individual in my_population:
-		print calculate_fitness(individual)
+	max_generations = 100
+	current_generation = 0
+
+	print crossover(my_population[0], my_population[1])
+	
+	while (not check_fitness(my_population) and current_generation < max_generations):
+		current_generation += 1
+		
+
+
+
+		
